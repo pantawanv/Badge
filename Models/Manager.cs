36b652b57c2e7fe3466a544666c2501e423c7c05
Celina Vang Henningsen <civah@hotmail.dk>
2023-11-23 15:35:32 +0100

    Added authentication

---
+++ Models/Manager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Badge.Models
+{
+    [Table("Manager")]
+    public class Manager
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser identityUser { get; set; }
+    }
+}
