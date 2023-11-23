326ecd622f29e273c4dcc7e00d4e7eb20db15a83
Celina Vang Henningsen <civah@hotmail.dk>
2023-11-23 14:50:15 +0100

    Changed Authentication

--- Badge/Models/Manager.cs
+++ Badge/Models/Manager.cs
@@ -1,7 +1,16 @@
-﻿namespace Badge.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Badge.Models
 {
+    [Table("Manager")]
     public class Manager
     {
+        [Key]
         public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
