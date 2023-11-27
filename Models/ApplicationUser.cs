ed7409789be270758f068f2e2ea6aaefcea7a2cc
Celina Vang Henningsen <civah@hotmail.dk>
2023-11-27 15:18:17 +0100

    Added Application User

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Badge.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        public string FName { get; set; }
+        [PersonalData]
+        public string LName { get; set; }
+    }
+}
