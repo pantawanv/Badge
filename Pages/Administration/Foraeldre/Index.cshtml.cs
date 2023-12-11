286c03c34db7c819dc51f3a0a5a00d5a161b8954
Celina Vang Henningsen <civah@hotmail.dk>
2023-12-11 13:07:17 +0100

    Fixed parent search

--- Pages/Administration/Foraeldre/Index.cshtml.cs
+++ Pages/Administration/Foraeldre/Index.cshtml.cs
@@ -53,7 +53,7 @@ namespace Badge.Pages.Administration.ParentAdmin
             if (!String.IsNullOrEmpty(searchString))
             {
                 parentsIQ = parentsIQ.Where(p => p.LName.Contains(searchString)
-                || p.FName.Contains(searchString) || p.Member.FullName.Contains(searchString)|| p.LName.Contains(searchString)
+                || p.FName.Contains(searchString) || p.LName.Contains(searchString)
                 || p.Phone.Contains(searchString) || p.Email.Contains(searchString)
                 );
 
