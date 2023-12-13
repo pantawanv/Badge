1b3f8ddd754ec00fa32c5a9cd8d4b6580f5c38cc
Celina Vang Henningsen <civah@hotmail.dk>
2023-12-13 19:12:01 +0100

    Achievements added

--- Pages/App/Index.cshtml.cs
+++ Pages/App/Index.cshtml.cs
@@ -1,12 +1,55 @@
+using Badge.Areas.Identity.Data;
+using Badge.Data;
+using Badge.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 
 namespace Badge.Pages.App
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public IndexModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
+            ApplicationDbContext context)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        public IList<Achievement> Achievements { get; set; }
+        public async Task OnGetAsync()
+        {
+            var achivements = await _context.Achievements.Include(a => a.AchievementType).ToListAsync();
+            if (achivements == null)
+            {
+
+            }
+            Achievements = achivements;
+        }
+
+        public bool CheckTicketAchievement (int amount)
+        {
+            return amount <= _context.Sales.Where(s => s.SellerId == _userManager.GetUserId(User)).Count();
+        }
+
+        public bool CheckChannelAchievement (int amount, string name)
+        {
+            switch (name) 
+            {
+                case "Mobile Pay":
+                    return _context.Sales.Where(s => s.SellerId == _userManager.GetUserId (User) && s.Channel.Name == "Mobile Pay").Count() > 0;
+                    break;
+                case "Kontant":
+                    return _context.Sales.Where(s => s.SellerId == _userManager.GetUserId (User) && s.Channel.Name == "Kontakt").Count() > 0;
+                    break;
+                    default: return false;
+                    break;
+            }
         }
     }
 }
