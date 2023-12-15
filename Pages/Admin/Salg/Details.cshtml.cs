aae5fe3dbf3992aff099eb23d7286a1514ea3e90
Celina Vang Henningsen <civah@hotmail.dk>
2023-12-15 11:30:15 +0100

    Achievements updated

--- Pages/Admin/Salg/Details.cshtml.cs
+++ Pages/Admin/Salg/Details.cshtml.cs
@@ -24,7 +24,7 @@ namespace Badge.Pages.Admin.SalesAdmin
                 return NotFound();
             }
 
-            var sale = await _context.Sales.Include(s => s.Seller).Include(s => s.Ticket).Include(s => s.Channel).FirstOrDefaultAsync(s => s.Id == id);
+            var sale = await _context.Sales.Include(s => s.Seller).ThenInclude(s => s.User).Include(s => s.Ticket).Include(s => s.Channel).FirstOrDefaultAsync(s => s.Id == id);
             if (sale == null)
             {
                 return NotFound();
